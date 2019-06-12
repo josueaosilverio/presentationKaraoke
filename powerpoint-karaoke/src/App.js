e7956802d9eae54fa784d8d371f86209be913100
Josué <josueaosilverio@gmail.com>
2019-06-12 19:33:03 +0100

    unsplash get working

--- powerpoint-karaoke/src/App.js
+++ powerpoint-karaoke/src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { toJson } from 'unsplash-js';
+import Unsplash from 'unsplash-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundNew: "https://www.placecage.com/c/200/300"
+    };
+  }
+
+  
+  componentWillMount() {
+    const unsplash = new Unsplash({
+      applicationId: "6107a66a250be4b140abbcbbb3ebcec3652af029a91dffa091c9e627d9e7bc06",
+      secret: "55074a7b3c1795a758e896e88dbbe1ea94ae59334b946c9017300cdd6716099c"
+    });
+  
+    unsplash.photos.getRandomPhoto({query:"videogame"})
+      .then(toJson)
+      .then(json => {
+        console.table(json.urls);
+        this.setState({ backgroundNew: json.urls.small });
+      })
+  }
+  
+
+
+
+
+
+
+
+  //VARIABLES
+
+
+  //UNSPLASH GET RAND PHOTO
+ 
+
+
+  render(){
+    return( <div className="App">
+    <img src={this.state.backgroundNew} alt="imagem"></img>
+  </div>)
+  };
 }
 
 export default App;
