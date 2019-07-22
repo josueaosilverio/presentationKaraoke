0372833700cdc65534a3c3972069f38547bf5349
Josué <josueaosilverio@gmail.com>
2019-07-22 17:02:57 +0100

    Fixes

--- powerpoint-karaoke/src/App.js
+++ powerpoint-karaoke/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Slide from './views/Slide'
+import Settings from './views/Settings';
 
 const App = () => {
 
   return (
     <div className="App" >
       <div>
-            <Slide></Slide>
+            <Settings></Settings>
+            {/* <Slide></Slide> */}
+            
       </div>
     </div>
   )
